dbbb5a0e8c98918b9872dbcab3be6e7e322c8cb5
llccing <lcf33123@foxmail.com>
2019-04-25 17:51:02 +0800

    update 名字调整

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ import React, {Component} from 'react';
 import { Text, View } from 'react-native'
 
 type Props = {};
-export default class App extends Component<Props> {
+export default class HelloWorldApp extends Component<Props> {
   render() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
