fe13a6798b476aca0d29bffeab359ba5b50a15f5
liuchunfeng <liuchunfeng@jd.com>
2019-04-28 21:02:48 +0800

    upddate react-navigation

--- App.js
+++ App.js
@@ -7,45 +7,62 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button} from 'react-native';
+import { createStackNavigator, createBottomTabNavigator, createAppContainer } from 'react-navigation'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-
-
-type Props = {};
-export default class App extends Component<Props> {
+class HomeScreen extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+        <Button title="go to Detail"
+          onPress={() => this.props.navigation.navigate('Details')}
+        >
+        </Button>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+class DetailScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Detail Screen</Text>
+        <Button
+          title="go to Details, 再去一次"
+          onPress={() => this.props.navigation.push('Details')}
+        />
+        
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+
+        <Button title="Go back" onPress={() => this.props.navigation.goBack()} />
+      </View>
+    )
+  }
+}
+
+// const AppNavigator = createStackNavigator({
+//   Home: HomeScreen,
+//   Details: DetailScreen,
+// }, {
+//   initialRouteName: 'Home'
+//   })
+
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeScreen,
+  Details: DetailScreen,
+}, {
+  initialRouteName: 'Home'
+  })
+
+const AppContainer = createAppContainer(AppNavigator)
+
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
