a02b6d0125a4e027245a5c2149057b020e5c277b
llccing <lcf33123@foxmail.com>
2019-09-18 22:34:23 +0800

    feature day5

--- src/main/days/day5.js
+++ src/main/days/day5.js
@@ -1,11 +1,50 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { Alert, StyleSheet, Text, Button, View } from 'react-native'
+import { MapView, Marker } from 'react-native-amap3d'
 
-export default class extends Component {
-  render() { 
+const styles = StyleSheet.create({})
+
+export default class MarkerExample extends Component {
+  _coordinates = [
+    {
+      latitude: 116.3702415368347,
+      longitude: 40.08144705283713,
+    },
+  ]
+
+  state = {
+    longitude: 116.3702415368347,
+    latitude: 40.08144705283713,
+  }
+
+  _changePosition() {
+    let longitude = this.state.longitude + 0.01
+    let latitude = this.state.latitude + 0.01
+    console.log(longitude)
+    this.setState({
+      longitude,
+      latitude,
+    })
+  }
+
+  render() {
     return (
-      <View>
-        <Text>txt </Text>
+      <View style={{ flex: 1 }}>
+        <MapView locationEnabled style={{ flex: 1 }}>
+          <Marker
+            active
+            title="家"
+            color="red"
+            description="哈哈，我到家了"
+            coordinate={{
+              latitude: this.state.latitude,
+              longitude: this.state.longitude,
+            }}
+          />
+        </MapView>
+        <View style={{ height: 50, alignItems: 'center', justifyContent: 'center' }}>
+          <Button title="我的位置" onPress={() => this._changePosition()}></Button>
+        </View>
       </View>
     )
   }
