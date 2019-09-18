24d059543b1c2ca66c3e69aac6f50b0c78fde98a
llccing <lcf33123@foxmail.com>
2019-09-18 22:33:39 +0800

    feature 增加高德地图依赖

--- ios/AwesomeProject/AppDelegate.m
+++ ios/AwesomeProject/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <AMapFoundationKit/AMapFoundationKit.h>
 
 @implementation AppDelegate
 
@@ -21,6 +22,8 @@
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+  
+  [AMapServices sharedServices].apiKey = @"b70e765523ed34de079cfad6a26b57c7";
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
